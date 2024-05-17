7b9813f3ed404a09b8781bde2cc45676a66516d6
princekwusu <princekowusu989@gmail.com>
2024-05-17 11:56:44 +0000

    updated db definition on snowflake

--- snowflakedb_def.sql
+++ snowflakedb_def.sql
@@ -1,74 +1,73 @@
---this script should be executed on snowflake
+--this script should be executed on snowflake worksheet
+
+--creating warehous
+CREATE WAREHOUSE CRYPTO_WH;
 
 --defining database
 CREATE DATABASE cryptodb;
 CREATE schema cryptoschema;
-CREATE TABLE cryptodb.cryptoschema.btcusd(
-open_time int,
-open float,
-high float,
-low float,
-close float,
-volume float,
-close_time int,
-quote_asset_volume float,
-num_trades int,
-taker_base_vol float,
-taker_quote_vol float,
-ignore int
+CREATE TABLE cryptodb.cryptoschema.btcusdt(
+Open_time timestamp,
+Open float,
+High float,
+Low float,
+Close float,
+Volume float,
+Quote_asset_volume float,
+Num_trades bigint,
+Taker_base_vol float,
+Taker_quote_vol float,
+Price_diff float,
+Price_change float
 );
 
 
 
 --select statement to verify table creation
-SELECT * FROM cryptodb.cryptoschema.btcusd LIMIT 10;
+SELECT * FROM cryptodb.cryptoschema.btcusdt;
 
 
---file format object creation
-CREATE SCHEMA file_format;
-
+--file format object creation     
+CREATE  or replace SCHEMA file_format;
+-- creation of file format
 CREATE OR REPLACE file format cryptodb.file_format.format_csv
     type = 'CSV'
     field_delimiter = ','
     RECORD_DELIMITER = '\n'
     skip_header = 1
-    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+
     
     
 
 --stage schema creation
-CREATE SCHEMA external_stage;
+CREATE or replace SCHEMA external_stage;
 
 --stage creation
 CREATE OR REPLACE STAGE cryptodb.external_stage.crypto_ext_stage 
     url="s3://transformedcryptodatabucket/"
-    credentials=(aws_key_id='keyid'
-    aws_secret_key='secretkey')
+    credentials=(aws_key_id='aws_access_keys'
+    aws_secret_key='aws_secrete key')
     FILE_FORMAT = cryptodb.file_format.format_csv;
 
+--listing available stages
 list @cryptodb.external_stage.crypto_ext_stage;
 
--- schema creation for snowpipe
 
+
+-- schema creation for snowpipe
 CREATE OR REPLACE SCHEMA cryptodb.snowpipe;
 
-
+--Create Pipe for market two data 
 CREATE OR REPLACE PIPE cryptodb.snowpipe.refineddatapipe
 AUTO_INGEST = TRUE
 AS
-COPY INTO cryptodb.cryptoschema.btcusd
+COPY INTO cryptodb.cryptoschema.btcusdt
 FROM @cryptodb.external_stage.crypto_ext_stage
 PATTERN = '.*refined_cryptodata.csv';
 
 
-
-
-
+--Describing  Pipe
 DESC PIPE cryptodb.snowpipe.refineddatapipe;
-
-TRUNCATE TABLE cryptodb.cryptoschema.btcusd;
-
-
 
 
 
